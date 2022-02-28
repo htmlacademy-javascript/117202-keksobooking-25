5f23309a25d12ecf111526cab7487696e1a66baa
Kbalod <kbalod@yandex.ru>
2022-02-28 18:10:11 +0300

    Добавил функции 2

--- js/main.js
+++ js/main.js
@@ -1,7 +1,13 @@
-let cat;
-let morphiy;
-let a = {
-
-  cat , morphiy
-
+const getRandomNumberFloat = function (min, max, precision) {
+  if (min >= 0 && max >= min) {
+    const value = Math.random();
+    return value.toFixed(precision);
+  }
 };
+
+const getRandomNumber = function (min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
+
+getRandomNumberFloat();
+getRandomNumber();
