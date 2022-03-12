42004c1739145a2098e0464e916736446e660b5a
Kbalod <kbalod@yandex.ru>
2022-03-12 08:11:32 +0300

    начало 7-го задания

---
+++ js/ad.js
@@ -0,0 +1,30 @@
+const transliteHome = {
+  flat : 'Квартира',
+  bungalow: 'Бунгало',
+  house: 'Дом',
+  palace: 'Дворец',
+  hotel: 'Отель',
+};
+const adUser = document.querySelector('#card').content;
+
+
+const generatorAd = function({author,offer}) {
+  const adElement = adUser.cloneNode(true);
+  adElement.querySelector('.popup__title').textContent = offer.title;
+  adElement.querySelector('.popup__text--address').textContent = offer.address;
+  adElement.querySelector('.popup__text--price').textContent = `${offer.price  } ₽/ночь`;
+  adElement.querySelector('.popup__type').textContent = transliteHome[offer.type];
+  adElement.querySelector('.popup__text--capacity').textContent = `${offer.rooms} комнаты для ${offer.guests} гостей`;
+  adElement.querySelector('.popup__text--time').textContent = `Заезд после ${offer.checkin}, выезд до ${offer.checkout}`;
+  const modifiers = offer.features.map((feature) => `popup__feature--${feature}`);
+  adElement.querySelectorAll('.popup__features').forEach((item) => {
+    if (! modifiers.includes(item.classList[1])){
+      item.remove();
+    }
+    adElement.querySelector('.popup__description').textContent = offer.description;
+    adElement.querySelector('.popup__photos').textContent = offer.photos;
+    document.querySelector('#map-canvas').appendChild(adElement);
+  });
+};
+
+export {generatorAd};
