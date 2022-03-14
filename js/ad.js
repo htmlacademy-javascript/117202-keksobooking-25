62d3948a98eea00c0b536b8d9e906b1726a2b882
Kbalod <kbalod@yandex.ru>
2022-03-14 16:48:45 +0300

    задание 7 часть 1

--- js/ad.js
+++ js/ad.js
@@ -7,7 +7,7 @@ const transliteHome = {
 };
 const adUser = document.querySelector('#card').content;
 
-const generatorAd = function({offer}) {
+const generatorAd = function({offer,author}) {
   const adElement = adUser.cloneNode(true);
   adElement.querySelector('.popup__title').textContent = offer.title;
   adElement.querySelector('.popup__text--address').textContent = offer.address;
@@ -15,17 +15,16 @@ const generatorAd = function({offer}) {
   adElement.querySelector('.popup__type').textContent = transliteHome[offer.type];
   adElement.querySelector('.popup__text--capacity').textContent = `${offer.rooms} комнаты для ${offer.guests} гостей`;
   adElement.querySelector('.popup__text--time').textContent = `Заезд после ${offer.checkin}, выезд до ${offer.checkout}`;
-
-  const modifiers = offer.features.map((feature) => `popup__feature--${feature}`);
-  adElement.querySelectorAll('.popup__features').forEach((item)=>{
+  const modifiers = offer.features.map((feature) => `popup__feature--${ feature}`);
+  adElement.querySelectorAll('.popup__feature').forEach((item) => {
     if (! modifiers.includes(item.classList[1])){
       item.remove();
     }
   });
-
-  //});
-  adElement.querySelector('.popup__description').textContent = offer.description;
-  adElement.querySelector('.popup__photos').innerHTML = offer.photos.map((photo) => `<img src='${photo}' class="popup__photo" width="45" height="40">`).join('');
+  if (offer.description !== undefined){
+    adElement.querySelector('.popup__description').textContent = offer.description;}
+  adElement.querySelector('.popup__photos').innerHTML = offer.photos.map((photo) => `<img src='${photo}' class="popup__photo" width="45" height="40">`);
+  adElement.querySelector('.popup__avatar').innerHTML = `<img src='${author.avatar}'`;
   document.querySelector('#map-canvas').appendChild(adElement);
 };
 
