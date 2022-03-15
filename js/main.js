de9c2097d1e342b90691940bdb06dbbcf183abc2
Kbalod <kbalod@yandex.ru>
2022-03-15 15:11:00 +0300

    задание 7 часть2

--- js/main.js
+++ js/main.js
@@ -1,8 +1,12 @@
 import {greatAds} from './data.js';
 import {generatorAd} from './ad.js';
+import {inactiveState, activateState} from './form.js';
 const ADS_LENGTH = 5;
 
 const randomAds = Array.from({length: ADS_LENGTH},greatAds);
 
 
 generatorAd(randomAds[1]);
+console.log(inactiveState, activateState);
+
+inactiveState();
