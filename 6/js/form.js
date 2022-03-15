1ce2b35e18651e1cd74608446bba52bfaf8241aa
Keks <keks@htmlacademy.ru>
2022-03-15 15:11:50 +0300

    :heavy_check_mark: Сборка #6

---
+++ 6/js/form.js
@@ -0,0 +1,20 @@
+const adform = document.querySelector('.ad-form');
+const fieldset = document.querySelectorAll('.fieldset');
+const mapfilter = document.querySelector('.map__filters');
+const select = document.querySelectorAll('.select');
+
+function inactiveState(){
+  adform.classList.add('ad-form--disabled');
+  fieldset.setAttribute('disabled', 'disabled');
+  mapfilter.classList.add('map__filters--disabled');
+  select.setAttribute('disabled', 'disabled');
+}
+
+function activateState(){
+  adform.classList.remove('ad-form--disabled');
+  fieldset.removeAttribute('disabled');
+  mapfilter.classList.remove('map__filters--disabled');
+  select.removeAttribute('disabled');
+}
+
+export {inactiveState,activateState};
---
+++ 6/js/util.js
@@ -0,0 +1,16 @@
+const getRandomNumberFloat = function (a, b, digits = 1) {
+  const lower = Math.min(Math.abs(a), Math.abs(b));
+  const upper = Math.max(Math.abs(a), Math.abs(b));
+  const result = Math.random() * (upper - lower) + lower;
+  return +result.toFixed(digits);
+};
+
+const getRandomNumber = function (a, b) {
+  const lower = Math.ceil(Math.min(Math.abs(a), Math.abs(b)));
+  const upper = Math.floor(Math.max(Math.abs(a), Math.abs(b)));
+  const result = Math.random() * (upper - lower + 1) + lower;
+  return Math.floor(result);
+};
+const getRandomArrayElement = (elements) => elements[getRandomNumber(0, elements.length - 1)];
+
+export {getRandomNumberFloat,getRandomNumber,getRandomArrayElement};
