85b2bd833417b6ad4f4e9dda0afc6bd03bd8a5dd
Keks <keks@htmlacademy.ru>
2022-03-21 14:46:59 +0300

    :heavy_check_mark: Сборка #7

---
+++ 7/js/form.js
@@ -0,0 +1,99 @@
+const ADFORM = document.querySelector('.ad-form');
+const FIELDSETS = ADFORM.querySelectorAll('fieldsets');
+const MAPFILTER = document.querySelector('.map__filters');
+const SELECTS = MAPFILTER.querySelectorAll('selects');
+const type = ADFORM.querySelector('[name="type"]');
+
+function deactivateState(){
+  ADFORM.classList.add('ad-form--disabled');
+  FIELDSETS.forEach((it) =>  {it.disabled = true;});
+  MAPFILTER.classList.add('map__filters--disabled');
+  SELECTS.forEach((it) =>  {it.disabled = true;});
+
+}
+
+function activateState(){
+  ADFORM.classList.remove('ad-form--disabled');
+  FIELDSETS.forEach((it) =>  {it.disabled = false;});
+  MAPFILTER.classList.remove('map__filters--disabled');
+  SELECTS.forEach((it) =>  {it.disabled = false;});
+}
+
+const pristine = new Pristine(ADFORM,{
+  classTo: 'ad-form__element' ,
+  errorClass: 'ad-form__element--invalid',
+  successClass: 'ad-form__element--valid',
+  errorTextParent: 'ad-form__element',
+  errorTextTag: 'span',
+  errorTextClass: 'error__message--text' ,
+});
+
+function validateTitle (value) {
+  return value.length >= 30 && value.length <= 100;
+}
+function validatePrice (value) {
+  return value.min >= 0 && value.max <= 100000;
+}
+pristine.addValidator(ADFORM.querySelector('#title'),validateTitle,'От 30 до 100 символов');
+
+const amountField = ADFORM.querySelector('#price');
+const minAmount = {
+  'bungalow': 0,
+  'flat': 1000,
+  'hotel': 3000,
+  'house': 5000,
+  'palace': 10000
+};
+function validateAmount (value) {
+  return value.length && parseInt(value) >= minAmount[type.value];
+}
+function getAmountErrorMessage () {
+  return `Не менее ${minAmount[type.value]} рублей`;
+}
+
+function onUnitChange () {
+  amountField.placeholder = minAmount[this.value];
+  amountField.min = minAmount[this.value];
+  pristine.addValidator(amountField, validateAmount ,getAmountErrorMessage);
+}
+
+ADFORM.querySelectorAll('[name="type"]').forEach((item) => item.addEventListener('change', onUnitChange));
+
+const rooms = ADFORM.querySelector('[name="rooms"]');
+const capacitys = ADFORM.querySelector('[name="capacity"]');
+const roomsOption = {
+  '1': ['1'],
+  '2': ['1', '2'],
+  '3': ['1', '2','3'],
+  '100': ['0']
+};
+
+function validateRoom () {
+  return roomsOption[rooms.value].includes(capacitys.value);
+}
+
+function getDeliveryErrorMessage () {
+  if (rooms.value !== '100' && capacitys.value !== '0'){return `для ${capacitys.value}-х гостей номер мал`;}
+  if (rooms.value === '100'){return '100 комнат для гостей не доступно';}
+  if (capacitys.value === '0'){return 'Без гостей доступно только 100 комнат';}
+}
+
+const timeInForm = ADFORM.querySelector('[name="timein"]');
+const timeOutForm = ADFORM.querySelector('[name="timeout"]');
+
+timeInForm.addEventListener('change', (evt) => {
+  timeOutForm.value = timeInForm.value;
+});
+timeOutForm.addEventListener('change', (evt) => {
+  timeInForm.value = timeOutForm.value;
+});
+
+pristine.addValidator(rooms, validateRoom, getDeliveryErrorMessage);
+pristine.addValidator(capacitys, validateRoom, getDeliveryErrorMessage);
+const pristineStart = function (){
+  ADFORM.addEventListener('submit', (evt) => {
+    evt.preventDefault();
+    pristine.validate();
+  });
+};
+export {deactivateState,activateState,pristineStart};
---
+++ 7/js/main.js
@@ -0,0 +1,12 @@
+import {greatAds} from './data.js';
+import {generatorAd} from './ad.js';
+import {deactivateState, activateState,pristineStart} from './form.js';
+const ADS_LENGTH = 5;
+
+const randomAds = Array.from({length: ADS_LENGTH},greatAds);
+
+
+generatorAd(randomAds[1]);
+console.log(deactivateState, activateState);
+
+pristineStart();
