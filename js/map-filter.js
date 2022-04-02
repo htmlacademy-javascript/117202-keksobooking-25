ac182478fdfe36316dc129384fbcf7ce21342fa1
Kbalod <kbalod@yandex.ru>
2022-04-02 19:48:00 +0300

    не получается

--- js/map-filter.js
+++ js/map-filter.js
@@ -3,75 +3,77 @@ import { createLoader } from './server-data.js';
 const allFilters = document.querySelector('.map__filters');
 const housingType = allFilters.querySelector('#housing-type');
 const housingPrice = allFilters.querySelector('#housing-price');
+const roomsNumber = allFilters.querySelector('#housing-rooms');
+const guestNumber = allFilters.querySelector('#housing-guests');
+const featuresFieldset = allFilters.querySelector('#housing-features');
+const checkedFeaturesItems = featuresFieldset.querySelectorAll('input :checked');
+const RERENDER_DELAY = 500;
+const debounce = function (callback, timeoutDelay = 500) {
+  let timeoutId;
+  return (...rest) => {
+    clearTimeout(timeoutId);
+    timeoutId = setTimeout(() => callback.apply(this, rest), timeoutDelay);
+  };
+};
+
 const onError = function(){
   return 'ошибка';
 };
 
 const type = function(it) {
-  const result = [];
-  for (let i=0;i<it.length;i++){
-    if(it[i].offer.type === housingType.value){
-      result.push(it[i]);
-    }
-  }
-  return result;
+  return it.offer.type === housingType.value || housingType.value === 'any';
 };
 const price = function(it){
-  const resultPrice = [];
   if(housingPrice.value === 'middle'){
-    for (let i=0;i<it.length;i++){
-      if(it[i].offer.price >10000 && it[i].offer.price < 50000){
-        resultPrice.push(it[i]);
-      }
-    }
+    return it.offer.price < 50000 && it.offer.price > 10000;
   }
   if(housingPrice.value === 'low'){
-    for (let i=0;i<it.length;i++){
-      if(it[i].offer.price < 10000){
-        resultPrice.push(it[i]);
-      }
-    }
+    return it.offer.price < 10000;
   }
   if(housingPrice.value === 'high'){
-    for (let i=0;i<it.length;i++){
-      if(it[i].offer.price > 50000){
-        resultPrice.push(it[i]);
-      }
-    }
+    return it.offer.price > 50000;
   }
-  return resultPrice;
+  return true;
 };
-const newMarkerFilter = function (it){
-  const resultType = [];
-  const resultPrice = [];
-  if(housingType.value !== 'any' && housingPrice.value === 'any'){
-    type(it).forEach((ix)=> {
-      resultType.push(ix);
-      newMarker(ix);
-    });
+const rooms = function(it) {
+  return (roomsNumber.value === it.offer.rooms.toString() || roomsNumber.value === 'any');
+};
+const guest = function(it){
+  if(guestNumber.value === 0){
+    return it.offer.guests.toString() === 100;
   }
-  if(housingType.value !== 'any' && housingPrice.value !== 'any'){
-    type(it).forEach((ix)=> {
-      resultType.push(ix);
-    });
+  if(guestNumber.value === 'any'){
+    return true;
   }
-  if(housingPrice.value !== 'any' && housingType.value === 'any'){
-    price(it).forEach((ixPrice)=> resultType.push(ixPrice));
-  }
-  if(housingPrice.value !== 'any' && housingType.value !== 'any'){
-    price(resultType).forEach((item)=> {
-      resultPrice.push(item);
-      newMarker(item);
-    });
-  }
-  console.log(resultType);
-  console.log(resultPrice);
-  //result.forEach((item)=> console.log(item));
+  return(guestNumber.value === it.offer.guests.toString());
 };
 
+const features = function(it){
+  Array.from(checkedFeaturesItems);
+  console.log(Array.from(checkedFeaturesItems));
+  Array.from(checkedFeaturesItems).every(function(elem) { return it.offer.features.includes(elem.value)});
+
+};
+
+const newMarkerFilter = function (it){
+  const result = [];
+  for(let i =0;i < it.length;i++){
+    if(type(it[i]) === true && price(it[i]) === true && rooms(it[i]) === true && guest(it[i]) === true && features(it[i]) === true){
+
+      result.push(it[i]);
+    }
+    if (result.length >=10) {
+      break;
+    }
+
+  }
+
+  result.forEach((element) => {
+    newMarker(element);
+  });};
 const dataFilterMap = createLoader(newMarkerFilter,onError);
 const clear = function(){
-  markerGroup.clearLayers();
+  debounce(markerGroup.clearLayers(),RERENDER_DELAY);
   dataFilterMap();
 };
 
